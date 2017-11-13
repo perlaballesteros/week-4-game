c97f8a0d7f6df7b35a0fdc60363473e1b50f601c
rubib <rubib@email.arizona.edu>
2017-11-12 21:10:42 -0700

    have almost finished need msg to pring

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,15 @@
-var numGiven=Math.floor((Math.random()*120)+19);
+//PerlaB. 11/12/2017
+
+var numGiven;
+var pink;
+var diamond;
+var purple;
+var colorful;
 var totalScore=0;
-var pink=Math.floor((Math.random()*12)+1);
-var diamond=Math.floor((Math.random()*12)+1);
-var purple=Math.floor((Math.random()*12)+1);
-var colorful=Math.floor((Math.random()*12)+1);
+var wins=0;
+var losses=0;
+
+generateRandoms();
 console.log("numGiven "+ numGiven);
 console.log("pink "+ pink);
 console.log("diamond "+ diamond);
@@ -11,7 +17,22 @@ console.log("purple "+ purple);
 console.log("colorful "+ colorful);
 
 
-$("#randnum").text(numGiven);
+//-------------------------------------
+function generateRandoms(){
+	
+	numGiven=Math.floor((Math.random()*120)+19);
+	$("#randnum").text(numGiven);
+	pink=Math.floor((Math.random()*12)+1);
+	diamond=Math.floor((Math.random()*12)+1);
+	purple=Math.floor((Math.random()*12)+1);
+	colorful=Math.floor((Math.random()*12)+1);
+	
+}	
+
+
+//--------------------------------------
+
+
 
 $(".imgs").on ("click",function(){
 
@@ -22,19 +43,47 @@ $(".imgs").on ("click",function(){
 	if(clicked==="pink")
 	{
 		totalScore+=pink;
+		console.log("pink "+ pink);
+		$("#totalscore").text(totalScore);
 		console.log("totalscore " +totalScore);
 	}
 	if(clicked==="diamond")
 	{
 		totalScore+=diamond;
+		$("#totalscore").text(totalScore);
 		console.log("totalscore " +totalScore);
 	}
-
 	if(clicked==="purple")
 	{
 		totalScore+=purple;
+		$("#totalscore").text(totalScore);
 		console.log("totalscore " +totalScore);
 	}
+	if(clicked==="colorful")
+	{
+		totalScore+=colorful;
+		$("#totalscore").text(totalScore);
+		console.log("totalscore " +totalScore);
+	}
+	if(totalScore===numGiven)
+	{
+		
+		wins++;
+		$("#winsShow").text("Wins: "+ wins);
+		generateRandoms();
+		totalScore=0;
+		
+		$("#totalscore").text(" ");
 
+	}
+	if(totalScore>numGiven)
+	{
+		
+		losses++;
+		$("#lossesShow").text("Losses: " +losses);
+		generateRandoms();
+		totalScore=0;
+		$("#totalscore").text(" ");
+	}
 
 });
